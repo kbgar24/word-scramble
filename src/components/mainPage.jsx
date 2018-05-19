8df52ca2a6494f6e683993cdf4a8b9e7aa0d2a5d
Kendrick Gardner <kbgar24@gmail.com>
2018-05-19 06:02:31 -0500

    Build scrambleLetters && seperateLettersByType

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -3,6 +3,22 @@ import React from 'react';
 const vowels = 'aeiouy'.split('');
 const consonants = 'bcdfghjklmnpqrstvwxz'.split('');
 
+const fisherYatesShuffle = array => {
+    let currentIndex = array.length;
+    let temporaryValue; 
+    let randomIndex;
+    
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+
 export default class Main extends React.Component {
 
   constructor() {
@@ -32,10 +48,40 @@ export default class Main extends React.Component {
     
   }
 
+  scrambleLetterList = () => {
+    const { letters:oldLetters } = this.state;
+    const letters = fisherYatesShuffle(oldLetters.slice());
+    this.setState({ letters });
+  }
+
+  seperateLettersByType = () => {
+    const { letters:oldLetters } = this.state;
+    const vowelArray = [];
+    const consonantArray = [];
+
+    oldLetters.forEach(letter => {
+      vowels.includes(letter)
+      ? vowelArray.push(letter)
+      : consonantArray.push(letter);
+    })
+
+    vowelArray.sort();
+    consonantArray.sort();
+
+    const letters = [...vowelArray, ...consonantArray];
+
+    this.setState({ letters })
+
+
+  }
+
   render = () => (
     <div>
       <h1>Word Scramble</h1>
       <button onClick={this.generateLetterList}>Generate Letters</button>
+      <button onClick={this.scrambleLetterList}>Scramble Letters</button>
+      <button onClick={this.seperateLettersByType}>Seperate Letters By Type</button>
+      
       <p>{ this.state.letters.join(' ') }</p>
     </div>
   );
