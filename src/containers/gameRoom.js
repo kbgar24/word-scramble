9140a0ffd948f881269849dab159860d889799b1
Kendrick Gardner <kbgar24@gmail.com>
2018-05-24 07:39:24 -0500

    Style gameplay section

--- src/containers/gameRoom.js
+++ src/containers/gameRoom.js
@@ -235,6 +235,34 @@ class GameRoom extends Component {
               </Table>
             </div>
 
+            <div className='hotKey'>
+              <h2>Game Play</h2>
+              <div className='admin-sep'></div>
+              <Table celled inverted selectable>
+                
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Action</Table.HeaderCell>
+                    <Table.HeaderCell>Hotkey</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                
+                <Table.Body>
+                  
+                  <Table.Row>
+                    <Table.Cell>Scramble Letters</Table.Cell>
+                    <Table.Cell> Option + F </Table.Cell>
+                  </Table.Row>
+
+                  <Table.Row>
+                    <Table.Cell>Seperate Vowels</Table.Cell>
+                    <Table.Cell>Option + J </Table.Cell>
+                  </Table.Row>
+
+                </Table.Body>
+              </Table>
+            </div>
+          
           </Grid.Column>
         
           <Grid.Column width={10}>
